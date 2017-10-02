e524375ebadad37a36d075ef8a0ddaa386bd4e89
cezanhanife <32462862+cezanhanife@users.noreply.github.com>
2017-10-02 20:14:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aaaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, toplam;
+            Console.Write("sayı gırınız:");
+            a = Convert.ToInt16(Console.ReadLine());
+            Console.Write("sayı gırınız:");
+            b = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("{0} ve {1}sayılarının toplamı:{2}", a,b,(a + b));
+            Console.ReadKey();
+
+
+        }
+    }
+}
